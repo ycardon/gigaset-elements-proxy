4f867787a2766c799cd80631dd657fd2edacb3bd
Yann Cardon <ycardon@gmail.com>
2019-04-11 14:20:23 +0200

    removed dependency on eventer

--- src/app.ts
+++ src/app.ts
@@ -9,23 +9,27 @@ require('console-stamp')(console, {colors: {stamp: 'grey', label: 'blue'}})
 require('source-map-support').install()
 process.on('unhandledRejection', console.log)
 
+// gigaset-element-proxy current version
 const VERSION = 'v2.0.0'
 
 // gigaset-element-proxy is starting
 console.info(`gigaset-element-provy ${VERSION} starting`)
 
 // authorize on gigaset API
-authorize()
+authorize( () => eventer.emit(eventer.AUTHORIZED) )
 
 // once authorized
 eventer.once(eventer.AUTHORIZED, ()=>{
-
+	
 	// start the local proxy
 	setImmediate(startWebserver)
-
+	
 	// publish the actual gigaset states
 	setImmediate(sendActualStates)
-
+	
 	// check peridically for new incoming gigaset events to publish
 	setInterval(checkEvents, conf('check_events_interval') * 1000)
 })
+
+// reauthorize periodically
+setTimeout(authorize, conf('auth_interval') * 60 * 1000)
--- src/gigaset.ts
+++ src/gigaset.ts
@@ -1,4 +1,4 @@
-import { eventer, conf } from './utils';
+import { conf } from './utils';
 import request = require('request')
 
 // a request wrapper that retains cookies
@@ -15,17 +15,16 @@ export enum GIGASET_URL {
 }
 
 /**
- * authorize every n minutes
+ * authorize on gigaset API
  */
-export function authorize(firstTime = true) {
+export function authorize(callback = ()=>{}) {
 	console.info('authorize on gigaset cloud api : starting')
 	gigasetRequest.post(GIGASET_URL.LOGIN, {form: {email: conf('email'), password: conf('password')}}, () => {
 		gigasetRequest.get(GIGASET_URL.AUTH, () => {
 			console.info('authorize on gigaset cloud api : done')
-			if (firstTime) eventer.emit(eventer.AUTHORIZED)
+			callback()
 		})
 	})
-	if (firstTime) setTimeout(authorize, conf('auth_interval') * 60 * 1000)
 }
 
 /**
@@ -36,5 +35,5 @@ export function authorize(firstTime = true) {
 export function handleGigasetError(functionName: string, error: object, body: string){
 	console.error(functionName + ' | unexpected error:', error)
 	console.error(functionName + ' | gigaset response:' + body)
-	authorize(false)
+	authorize()
 }
